bb0d09bed5a8d1bef4d58907783f5f3c4bff721e
ShaelShah <shael.shah7@gmail.com>
2017-04-15 15:14:51 -0400

    Initial Commit

--- app/src/main/java/com/shael/shah/expensemanager/MainActivity.java
+++ app/src/main/java/com/shael/shah/expensemanager/MainActivity.java
@@ -3,11 +3,21 @@ package com.shael.shah.expensemanager;
 import android.app.Activity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
+
+    private ArrayList<Expense> expenses;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expenses = getListFromSharedPreferences();
+    }
+
+    public ArrayList<Expense> getListFromSharedPreferences() {
+        return new ArrayList<>();
     }
 }
